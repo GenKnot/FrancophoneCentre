ac0b45b77f9d1cb03f6962988fd447d28d09cc2d
GenKnot <167164229+GenKnot@users.noreply.github.com>
2025-06-16 18:31:09 -0400

    Update 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ['en', 'fr', 'zh-hans', 'zh-hant'],
-        defaultLocale: 'en',
-        localeDetection: false,
-    },
-
     async rewrites() {
         return [
             {
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,13 +7,11 @@ export const metadata: Metadata = {
 	description: "抱歉，您访问的页面不存在",
 	keywords: "404, 页面未找到, 法语中心, QFEC",
 };
- 
-const index = () => {
-  return (
-    <Wrapper>
-      <Error />
-    </Wrapper>
-  );
-};
 
-export default index;
+export default function NotFound() {
+	return (
+		<Wrapper>
+			<Error />
+		</Wrapper>
+	);
+} 
